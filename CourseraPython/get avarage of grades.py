d96bd4dac3388a5be92aa3cc2e8f61a372285b0b
Ahmed Mansour <154812224+AhmedMansour024@users.noreply.github.com>
2024-02-21 18:12:37 +0000

    New Update

--- CourseraPython/get avarage of grades.py
+++ CourseraPython/get avarage of grades.py
@@ -1,5 +1,5 @@
-# grade1=75
-# grade2=65
+grade1=75
+grade2=65
 
 # def garade_statue(grade1, grade2):
 #     '''(num ,num) -> num
@@ -8,58 +8,58 @@
 #     return passed grade if passed in one grade and
 #     return 0.0 if not passed in both. ''' 
 
-# #accemulatores for grades
-# total = 0
-# grade_count = 0
+# accemulatores for grades
+total = 0
+grade_count = 0
 
 
-# if grade1 >= 50 and grade2 >= 50:
-#     total = grade1 + grade2
-#     grade_count = 2
+if grade1 >= 50 and grade2 >= 50:
+    total = grade1 + grade2
+    grade_count = 2
 
-# if grade_count > 0:
-#     print(total / grade_count)
-# else:
-#     print(0.0)
-# ####################    
+if grade_count > 0:
+    print(total / grade_count)
+else:
+    print(0.0)
+####################    
 
-# if grade1 >= 50:
-#     total = total + grade1
-#     grade_count = grade_count + 1
-# else:
-#     total = total + grade2
-#     grade_count = grade_count + 1
+if grade1 >= 50:
+    total = total + grade1
+    grade_count = grade_count + 1
+else:
+    total = total + grade2
+    grade_count = grade_count + 1
 
-# if grade_count > 0:
-#     print(total / grade_count)
-# else:
-#     print(0.0)
-# ####################
+if grade_count > 0:
+    print(total / grade_count)
+else:
+    print(0.0)
+####################
 
-# if grade1 >= 50:
-#     total = total + grade1
-#     grade_count = grade_count + 1
-# elif grade2 >= 50:
-#     total = total + grade2
-#     grade_count = grade_count + 1
+if grade1 >= 50:
+    total = total + grade1
+    grade_count = grade_count + 1
+elif grade2 >= 50:
+    total = total + grade2
+    grade_count = grade_count + 1
 
-# if grade_count > 0:
-#     print(total / grade_count)
-# else:
-#     print(0.0)
+if grade_count > 0:
+    print(total / grade_count)
+else:
+    print(0.0)
     
-# ###############
-# if grade1 >= 50:
-#     total = total + grade1
-#     grade_count = grade_count + 1
-# if grade2 >= 50:
-#     total = total + grade2
-#     grade_count = grade_count + 1
+###############
+if grade1 >= 50:
+    total = total + grade1
+    grade_count = grade_count + 1
+if grade2 >= 50:
+    total = total + grade2
+    grade_count = grade_count + 1
 
-# if grade_count > 0:
-#     print(total / grade_count)
-# else:
-#     print(0.0)    
+if grade_count > 0:
+    print(total / grade_count)
+else:
+    print(0.0)    
 
 
 
